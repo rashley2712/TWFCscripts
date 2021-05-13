096a79b2dbabde4e8386859e661d3421ef2e544b
Richard Ashley <rashley@gmail.com>
2021-05-13 14:20:49 +0200

    adding

---
+++ chornay.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+import argparse, subprocess, sys, signal
+
+commandHistory = None
+commandCounter = 1
+
+
+def executeCommand(commandString):
+	global commandHistory, commandCounter
+	print("[step %d] : %s"%(commandCounter, commandString))
+	commandCounter+=1
+	commandHistory.write(commandString)
+	commandHistory.write("\n")
+	commandHistory.flush()
+	fields = [str(c) for c in commandString.split()]
+	subprocess.call(fields)
+	
+def readFocusTable(filename):
+	focusTable = {}
+	focusFile = open(args.focus, 'rt')
+	for line in focusFile:
+		line = line.strip()
+		if len(line)<3: continue
+		if line[0]=='#': continue
+		fields = line.split()
+		filterName = str(fields[0])
+		focusValue = float(fields[1])
+		focusTable[filterName] = focusValue
+	# print(focusTable)
+	return focusTable
+
+def signal_handler(sig, frame):
+	global commandHistory
+	if commandHistory is not None:
+		print("command history in command.log")
+		commandHistory.close()
+	sys.exit(-1)
+
+if __name__ == "__main__":
+	signal.signal(signal.SIGINT, signal_handler)
+	parser = argparse.ArgumentParser(description='Performs a block observation for SW21021a07 (Chornay).')
+	parser.add_argument('-t', '--target', type=str, help='Target name. (Must match the name in the TCS catalog). Optional... if not specified the script will not issue the ''GOCAT'' command')	
+	parser.add_argument('-f', '--filters', type=str, nargs="+", help="Filters")
+	parser.add_argument('-c', '--camera', type=str, default="TWFC2", help="Which camera to use? TWFC1 or TWFC2. Default: TWFC2")
+	parser.add_argument('--focus', type=str, default="focus.dat", help="Filename for a text file containing focus values (in mm). Default 'focus.dat'.")
+	parser.add_argument('-n', '--numexps', type=int, nargs="+", help="Number of exposures for each filter.")
+	parser.add_argument('-e', '--exptimes', type=float, nargs="+", help="Exposure times for each filter.")
+	parser.add_argument('-b', '--binning', type=int, default=4, help="Binning. Default is 4x4.")
+	args = parser.parse_args()
+	# print(args)
+	
+	skipGOCAT = False
+	if args.target is None:
+		skipGOCAT = True
+		args.target = "<current pointing>"
+	camera = args.camera
+	binning = args.binning
+	focusDat = args.focus
+
+	if args.filters is None:
+		print("Please specify which filters you want with -f [filters]. Separate each filter with a space.")
+		sys.exit()
+	filters = args.filters
+	
+	if args.exptimes is None:
+		print("Please specify the exposure times for each of the filters. Separate each time with a space.")
+		sys.exit()
+	expTimes = args.exptimes
+	
+	if args.numexps is None:
+		print("Please specify the number of exposures for each of the filters. Separate each 'n' with a space.")
+		sys.exit()
+	numExps = args.numexps
+
+	# Read the focus values
+	focusTable = readFocusTable(args.focus)
+
+	# Present a summary to the observer
+	print("OBSERVATION BLOCK\n+++++++++++++++++\n")
+	print("Target: %s\tCamera: %s\tbinning: %d"%(args.target, camera, binning))
+	print()
+	for (f, e, n) in zip(filters, expTimes, numExps):
+		print("\tfilter: %s\tfocus: %.2f\texptime: %.2f\tnumexposures: %d"%(f, focusTable[f], e, n))
+	print()
+	if input("Does this look correct? (y/n) ") != "y":
+		sys.exit()
+
+	commandHistory = open("command.log", "wt")
+
+	# GOCAT to the target
+	if not skipGOCAT:
+		executeCommand("gocat %s"%args.target)
+
+	# Run through the observing block
+	for f, e, n in zip(filters, expTimes, numExps):
+
+		# Set the filter
+		executeCommand("indicam filter %s -f %s"%(camera, f))
+
+		# Set the focus
+		focusValue = focusTable[f]	
+		executeCommand("focus %.2f"%(focusValue))
+
+		executeCommand("indicam -b %d run %s %.2f -n %d"%(binning, camera, e, n))
+	
+	print("command history in command.log")
+	commandHistory.close()
+
+	sys.exit()
